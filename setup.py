0d9fccd8c8cfbd8483c26555b7fa4a56ade0e246
Josuah Aron <josuaharon@hotmail.com>
2020-04-25 18:11:42 +0000

    Add the twine command to upload to pyp

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ Wheel format:
     python setup.py bdist_wheel --dist-dir=./dist && rm -rf ./*.egg-info
 
 Upload to Pypi:
-    pip install twine
+    pip install -U twine wheel setuptools
+    twine check dist/*
     twine upload dist/*
 """
 from setuptools import setup
